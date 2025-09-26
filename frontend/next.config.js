07077f74048fd10e05e71421e1ec993be1915784
vashsempai <72339867+vashsempai@users.noreply.github.com>
2025-09-27 01:02:40 +0500

    Create next.config.js

---
+++ frontend/next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true
+  },
+  images: {
+    domains: ['localhost']
+  },
+  env: {
+    CUSTOM_KEY: process.env.CUSTOM_KEY
+  }
+}
+
+module.exports = nextConfig
