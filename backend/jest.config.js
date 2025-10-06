7141c0c3e9d89c3eccfaadfcec4bd1e380fa2b9b
vashsempai <72339867+vashsempai@users.noreply.github.com>
2025-10-06 06:23:34 +0500

    Update jest.config.js

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -5,11 +5,12 @@ const config = {
   // Explicitly tell Jest that we are using ES Modules.
   // This helps Jest understand `import`/`export` syntax.
   transform: {},
+  // Load global setup file before any test files
+  setupFiles: ['<rootDir>/tests/setupTests.js'],
   // The bail config option can be used to stop Jest from running further tests
   // after the first test has failed.
   bail: 1,
   // Make calling deprecated APIs throw helpful error messages
   errorOnDeprecated: true,
 };
-
 export default config;
